ae771d4b7e390d672a36c5379c64636cea600ec8
platane <me@platane.me>
2022-09-11 17:49:10 +0200

    ✨ surfer movement

--- src/engine/stepSurfer.ts
+++ src/engine/stepSurfer.ts
@@ -5,15 +5,30 @@ import {
   getScreenX,
   getScreenY,
 } from "../renderer/utils/raycast";
-import { getVec3, positions as particlesPositions } from "./buffers";
+import { positions as particlesPositions } from "./buffers";
 import { ITEM_RADIUS, nPhysic } from "./constants";
 
 export const lookAt = vec3.create();
 const o = vec3.create();
 const v = vec3.create();
-const p = vec3.create();
 
-export const onTap: Handler = () => {};
+let acc = 0;
+let accCoolDown = 0;
+let accQ = false;
+
+const doAcc = () => {
+  accQ = false;
+  acc = 1;
+  accCoolDown = 0.2;
+  console.log("acc");
+};
+
+export const onTap: Handler = () => {
+  if (accCoolDown > 0) {
+    accQ = true;
+    return;
+  } else doAcc();
+};
 export const onMouseMove: Handler = (touches) => {
   const { pageX, pageY } = touches[0];
 
@@ -46,56 +61,46 @@ const probeDepth = (x: number, z: number) => {
     if (d2Square < r ** r) closest.push(i);
   }
 
-  if (closest.length === 0) return 0;
+  if (closest.length === 0) return 1.5;
 
   const ys = closest.map((i) => particlesPositions[i * 3 + 1]).sort();
 
-  return ys.slice(0, 2).reduce((sum, y, _, { length }) => sum + y / length, 0);
-
-  const y0 =
-    closest.reduce(
-      (max, i) => Math.max(max, particlesPositions[i * 3 + 1]),
-      -10
-    ) + ITEM_RADIUS;
-
-  return y0;
-
-  const weights = closest.map(
-    (i) =>
-      1 /
-      Math.hypot(
-        ITEM_RADIUS,
-        Math.hypot(
-          particlesPositions[i * 3 + 0] - x,
-          particlesPositions[i * 3 + 1] - y0,
-          particlesPositions[i * 3 + 2] - z
-        )
-      )
-  );
-
   return (
-    closest.reduce(
-      (sum, i, j) => sum + particlesPositions[i * 3 + 1] * weights[j],
-      0
-    ) /
-      weights.reduce((sum, x) => sum + x) +
+    ys.slice(-2).reduce((sum, y, _, { length }) => sum + y / length, 0) +
     ITEM_RADIUS
   );
 };
 
+const up = vec3.create();
+up[2] = 1;
+
 export const stepSurfer = (dt: number) => {
-  vec3.scaleAndAdd(velocity, velocity, acceleration, dt);
-  vec3.scaleAndAdd(position, position, velocity, dt);
-
-  position[1] = probeDepth(position[0], position[2]);
-
+  // rotate board
   vec3.sub(v, lookAt, position);
   v[1] = 0;
   vec3.normalize(v, v);
 
-  const up = vec3.create();
-  up[2] = 1;
-
   quat.identity(rotation);
   quat.rotationTo(rotation, up, v);
+
+  //
+  //
+  //
+  acceleration.fill(0);
+
+  // solid friction
+  vec3.scaleAndAdd(acceleration, acceleration, velocity, -0.12 / dt);
+
+  // tick acc
+  accCoolDown = Math.max(0, accCoolDown - dt);
+  if (accCoolDown === 0 && accQ) doAcc();
+
+  vec3.scaleAndAdd(acceleration, acceleration, v, acc * 100);
+  acc = acc * 0.85;
+
+  // resolution
+  vec3.scaleAndAdd(velocity, velocity, acceleration, dt);
+  vec3.scaleAndAdd(position, position, velocity, dt);
+
+  position[1] = probeDepth(position[0], position[2]);
 };

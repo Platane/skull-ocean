56e0d1775b7d14ae27c2f63223f139b7cb4be2ff
platane <me@platane.me>
2022-09-12 22:47:32 +0200

    ✨ improve tap detection

--- src/controls.ts
+++ src/controls.ts
@@ -1,26 +1,35 @@
 import { canvas } from "./canvas";
-import { onTouchStart, onTouchMove, onTouchEnd } from "./renderer/camera";
+import {
+  onTouchStart as onTouchStart_camera,
+  onTouchMove as onTouchMove_camera,
+  onTouchEnd as onTouchEnd_camera,
+} from "./renderer/camera";
 import { Handler } from "./controls-type";
 import {
   onMouseMove as onMouseMove_surfer,
   onTap as onTap_surfer,
 } from "./engine/stepSurfer";
 
-let downTimeStamp = 0;
-let t0: { pageX: number; pageY: number }[];
+let tap: { pageX: number; pageY: number; timeStamp: number } | null = null;
 const onStart: Handler = (t) => {
-  downTimeStamp = Date.now();
-  t0 = t;
-  onTouchStart(t);
+  if (t[0])
+    tap = { pageX: t[0].pageX, pageY: t[0].pageY, timeStamp: Date.now() };
+
+  onTouchStart_camera(t);
 };
 const onMove: Handler = (t) => {
-  onTouchMove(t);
+  if (tap && Math.hypot(t[0].pageX - tap.pageX, t[0].pageY - tap.pageY) > 40)
+    tap = null;
+
+  onTouchMove_camera(t);
   onMouseMove_surfer(t);
 };
 const onEnd: Handler = (t) => {
-  onTouchEnd(t);
+  onTouchEnd_camera(t);
 
-  if (Date.now() < downTimeStamp + 300) onTap_surfer(t);
+  if (tap && Date.now() < tap.timeStamp + 300) {
+    onTap_surfer(t);
+  }
 };
 
 canvas.addEventListener("mousedown", (event) => onStart([event]));
